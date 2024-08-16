5f5f3f314076878e1eaa48dc46c408e8fd148222
Bhavik Kothari <20669150+bkkothari2255@users.noreply.github.com>
2024-08-17 05:12:40 +0530

    Added obtain_auth_token route

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu-items',views.MenuItemsView.as_view()),
@@ -8,5 +9,6 @@ urlpatterns = [
     path('orders',views.OrdersView.as_view()),
     path('orders/<int:pk>',views.SingleOrderView.as_view()),
     path('cart/menu-items',views.CartView.as_view()),
-    path('secret/',views.secret)
+    path('secret/',views.secret),
+    path('api-auth-token/', obtain_auth_token) # only accepts http post call
 ]
