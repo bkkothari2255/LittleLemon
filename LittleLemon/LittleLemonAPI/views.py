5a315d85a71cb23d5b765a8f50aa31db5d0dee82
Bhavik Kothari <20669150+bkkothari2255@users.noreply.github.com>
2024-08-17 22:17:46 +0530

    Added permissions to menu item and category item views

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,33 +1,70 @@
-from rest_framework.response import Response
-from rest_framework import generics, viewsets
+from django.http import JsonResponse
+from .pagination import MenuItemsPagination, CategoryPagination
+from .permissions import IsDeliveryCrew, IsManager
+from rest_framework import generics
 from .models import MenuItem, Category, OrderItem, Cart
 from .serializers import CategorySerializer, MenuItemSerializer, OrderSerializer, CartSerializer
-from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import permission_classes, throttle_classes
+#from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+#from rest_framework.decorators import permission_classes, throttle_classes
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from .throttles import TenCallsPerMinute
+from django.contrib.auth.models import Group, User
+from rest_framework import viewsets
 
 class CategoryView(generics.ListCreateAPIView):
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = CategoryPagination
+    
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != "GET":
+            permission_classes = [IsAuthenticated, IsAdminUser]
+                   
+        return [permission() for permission in permission_classes]
+            
 
 class MenuItemsView(generics.ListCreateAPIView):
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
-    permission_classes= [IsAuthenticated]
     queryset = MenuItem.objects.select_related('category').all()
     serializer_class = MenuItemSerializer
     search_fields = ['title','category__title']
     ordering_fields = ['price','category']
     filterset_fields = ['category']
+    pagination_class = MenuItemsPagination
+    
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != "GET":
+            permission_classes = [IsAuthenticated, IsAdminUser]  
+                     
+        return [permission() for permission in permission_classes] 
     
 class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
-    permission_classes= [IsAuthenticated]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
     
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method == "GET":
+            permission_classes = [IsAuthenticated]  
+        elif self.request.method == "DELETE":
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        elif self.request.method == "PATCH":
+            permission_classes = [IsAuthenticated, IsManager]
+        
+        return [permission() for permission in permission_classes]
+    
+    def patch(self,request, *args, **kwargs):
+        menu_item = MenuItem.objects.get(pk=self.kwargs['pk'])
+        menu_item.featured = not menu_item.featured
+        menu_item.save()
+        return JsonResponse(status=200, data={'message':'Featured status of {} changed to {}'.format(str(menu_item.title) ,str(menu_item.featured))})
+
+        
 class OrdersView(generics.ListCreateAPIView):
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
     permission_classes= [IsAuthenticated]
@@ -40,12 +77,26 @@ class SingleOrderView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = OrderItem.objects.all()
     serializer_class = OrderSerializer
     
-class CartView(generics.RetrieveUpdateDestroyAPIView):
+class CartView(generics.ListCreateAPIView,generics.RetrieveUpdateDestroyAPIView):
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
     permission_classes= [IsAuthenticated]
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
     
+
+class DeliveryCrewView(generics.RetrieveUpdateAPIView):
+    throttle_classes = [AnonRateThrottle,UserRateThrottle]
+    permission_classes = [IsDeliveryCrew]
+    
+
+class ManagerView(generics.ListCreateAPIView):
+    permission_classes = [IsManager]
+    
+    
+class UserGroupViewSet(viewsets.ViewSet):
+    permission_classes = [IsAdminUser]
+    
+
 
 # @api_view()
 # @permission_classes([IsAuthenticated])
