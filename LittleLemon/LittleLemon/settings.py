33b52664068554e8194f755d49fd187ef2fd8f7b
Bhavik Kothari <20669150+bkkothari2255@users.noreply.github.com>
2024-08-17 21:53:50 +0530

    Disable jwt and page size

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -149,7 +149,6 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
     'DEFAULT_AUTHENTICATION_CLASSES':[
         #'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
