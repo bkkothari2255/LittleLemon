2828b0a0f53c9776cc560fce85cbd3853d52a3ab
Bhavik Kothari <20669150+bkkothari2255@users.noreply.github.com>
2024-08-17 05:58:52 +0530

    Fixed category selection issue

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -14,7 +14,7 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     featured = models.BooleanField(db_index=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,default=1)
     
     def __str__(self):
         return self.title
--- LittleLemon/LittleLemonAPI/serializers.py
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -9,13 +9,13 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id','slug','title']
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     #category = CategorySerializer()
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     
     class Meta:
         model = MenuItem
         fields = ['id','title','price','featured','price_after_tax','category']
-        depth = 1
         
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)
