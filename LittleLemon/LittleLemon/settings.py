83dada4e374fce72971fdae420ff0ebbb1b27a68
Bhavik Kothari <20669150+bkkothari2255@users.noreply.github.com>
2024-08-17 08:54:26 +0530

    Added JWT features

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'djoser',
     'debug_toolbar',
     'django_filters',
+    'rest_framework_simplejwt'
     
 ]
 
@@ -148,6 +150,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
     'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
@@ -165,4 +168,8 @@ REST_FRAMEWORK = {
 DJOSER = {
     "USER_ID_FIELD":"username"
     
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
 }
